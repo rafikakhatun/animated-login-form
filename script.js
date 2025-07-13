dba80b70d525d83629fc3e0dbe184fe218e0b5b4
rafika khatun <rafikakhatun607@gmail.com>
2025-07-13 20:09:58 +0530

    Select DOM elements for email, password and error spans

---
+++ script.js
@@ -0,0 +1,5 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById("password-error");
+const loginBtn = document.getElementById("btn");
